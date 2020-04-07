f88ce31b9f9036ddb208012674024c668761ecc7
Travis CI User <travis@example.org>
2020-04-07 09:56:34 +0000

    fix(import): Remove incorrect import

--- js/Register.js
+++ js/Register.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{828:function(e,n,a){"use strict";a.r(n);var t=a(789),l=a(2),c=a.n(l),r=a(790),u=a(516);n.default=function(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(r.a,{title:"Patch"}),c.a.createElement(t.Main,null,c.a.createElement("div",null,c.a.createElement(u.a,null))))}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{833:function(e,n,a){"use strict";a.r(n);var t=a(794),l=a(2),c=a.n(l),r=a(795),u=a(516);n.default=function(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(r.a,{title:"Patch"}),c.a.createElement(t.Main,null,c.a.createElement("div",null,c.a.createElement(u.a,null))))}}}]);
 //# sourceMappingURL=Register.js.map
