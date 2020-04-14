6826d4ac028b6b424b81cae7e69c823f82d6afe0
Travis CI User <travis@example.org>
2020-04-14 07:32:08 +0000

    chore(release): 0.6.3
    ## [0.6.3](https://github.com/RedHatInsights/patchman-ui/compare/v0.6.2...v0.6.3) (2020-04-09)
    
    ### Bug Fixes
    
    * **text-style:** Wrap and truncate expandable description ([f1ee036](https://github.com/RedHatInsights/patchman-ui/commit/f1ee0367f064c5b7f66925082a4382b070d22f23))

--- js/Register.js
+++ js/Register.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{828:function(e,n,a){"use strict";a.r(n);var t=a(789),l=a(2),c=a.n(l),r=a(790),u=a(516);n.default=function(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(r.a,{title:"Patch"}),c.a.createElement(t.Main,null,c.a.createElement("div",null,c.a.createElement(u.a,null))))}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{834:function(e,n,a){"use strict";a.r(n);var t=a(795),l=a(2),c=a.n(l),r=a(796),u=a(517);n.default=function(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(r.a,{title:"Patch"}),c.a.createElement(t.Main,null,c.a.createElement("div",null,c.a.createElement(u.a,null))))}}}]);
 //# sourceMappingURL=Register.js.map
