d0489d4906c87f5c3a44e1c4c40892fd1698a161
Travis CI User <travis@example.org>
2020-06-08 11:18:40 +0000

    Update snapshots to match new FEC

--- js/Register.js
+++ js/Register.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{485:function(e,n,a){"use strict";a.r(n);var t=a(487),l=a(0),c=a.n(l),r=a(488),u=a(239);n.default=function(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(r.a,{title:"Patch"}),c.a.createElement(t.Main,null,c.a.createElement("div",null,c.a.createElement(u.a,null))))}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{459:function(e,n,a){"use strict";a.r(n);var t=a(461),l=a(0),c=a.n(l),r=a(462),u=a(223);n.default=function(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(r.a,{title:"Patch"}),c.a.createElement(t.Main,null,c.a.createElement("div",null,c.a.createElement(u.a,null))))}}}]);
 //# sourceMappingURL=Register.js.map
