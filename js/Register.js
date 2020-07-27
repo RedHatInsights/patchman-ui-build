046c3b20a514f624e3bd2921299d2fb62bd754c4
Travis CI User <travis@example.org>
2020-07-27 12:53:52 +0000

    fix(revert): Revert "feat(advisoryDetail): Add information about CVEs"
    This reverts commit 17d9571076ea8e2d202e2dc92822da2f271f1063.

--- js/Register.js
+++ js/Register.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{779:function(e,n,a){"use strict";a.r(n);var t=a(783),l=a(0),c=a.n(l),r=a(785),u=a(421);n.default=function(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(r.a,{title:"Patch"}),c.a.createElement(t.Main,null,c.a.createElement("div",null,c.a.createElement(u.a,null))))}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{779:function(e,n,a){"use strict";a.r(n);var t=a(783),l=a(0),c=a.n(l),r=a(784),u=a(421);n.default=function(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(r.a,{title:"Patch"}),c.a.createElement(t.Main,null,c.a.createElement("div",null,c.a.createElement(u.a,null))))}}}]);
 //# sourceMappingURL=Register.js.map
