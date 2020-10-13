4948819aa0cb405fe3d2900ca700b977a8b6c387
Travis CI User <travis@example.org>
2020-10-13 08:22:44 +0000

    chore(hotfix): disable system-packages

--- js/Register.js
+++ js/Register.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{830:function(e,n,a){"use strict";a.r(n);var t=a(849),l=a(0),c=a.n(l),r=a(851),u=a(445);n.default=function(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(r.a,{title:"Patch"}),c.a.createElement(t.Main,null,c.a.createElement("div",null,c.a.createElement(u.a,null))))}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{691:function(e,n,a){"use strict";a.r(n);var t=a(696),l=a(0),c=a.n(l),r=a(698),u=a(387);n.default=function(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(r.a,{title:"Patch"}),c.a.createElement(t.Main,null,c.a.createElement("div",null,c.a.createElement(u.a,null))))}}}]);
 //# sourceMappingURL=Register.js.map
