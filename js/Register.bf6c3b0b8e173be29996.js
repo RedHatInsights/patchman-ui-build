b478dcddf6222543c261465a020d985aef5476c0
Travis CI User <travis@example.org>
2021-01-04 11:11:27 +0000

    chore(release): 1.3.1
    ## [1.3.1](https://github.com/RedHatInsights/patchman-ui/compare/v1.3.0...v1.3.1) (2020-12-21)
    
    ### Bug Fixes
    
    * **titles:** add dynamic titles ([c957ca0](https://github.com/RedHatInsights/patchman-ui/commit/c957ca0e84be1b8e8398f719fad2cdd242eb5b71))

--- js/Register.bf6c3b0b8e173be29996.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{1241:function(e,n,a){"use strict";a.r(n);var t=a(1055),l=a(0),c=a.n(l),r=a(1057),u=a(245);n.default=function(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(r.a,{title:"Patch"}),c.a.createElement(t.Main,null,c.a.createElement("div",null,c.a.createElement(u.a,null))))}}}]);
-//# sourceMappingURL=Register.bf6c3b0b8e173be29996.js.map
