080374e81564497bc5ea5dfcedb52966fd6580bc
Travis CI User <travis@example.org>
2021-07-01 09:08:13 +0000

    chore(release): 1.24.0
    # [1.24.0](https://github.com/RedHatInsights/patchman-ui/compare/v1.23.0...v1.24.0) (2021-07-01)
    
    ### Features
    
    * **PackageSystems:** SPM-960 enable checkbox ([cc9b30c](https://github.com/RedHatInsights/patchman-ui/commit/cc9b30c15a2cfabb1009320d287ab347491a724e))

--- js/Register.ba4d23a8ad15662a0e9d.js
+++ js/Register.ba4d23a8ad15662a0e9d.js
@@ -1,2 +1,2 @@
 (self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman=self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman||[]).push([[494],{87810:(e,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>u});var r=t(86350),a=t(77865),l=t.n(a),c=t(49642),s=t(31507),_=t(32132),o=t(30893);const u=function(){return l().createElement(l().Fragment,null,l().createElement(c.Z,{title:_.N.formatMessage(o.Z.generalAppName)}),l().createElement(r.Z,null,l().createElement("div",null,l().createElement(s.Q,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/Register.588642aa2ea7edbc3c93.js.map
+//# sourceMappingURL=../sourcemaps/Register.48b2ca732429154b3a8a.js.map
