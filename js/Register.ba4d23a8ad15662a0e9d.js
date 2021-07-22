929cb9fea5819884584fa5efb85fd9c013069ff3
Travis CI User <travis@example.org>
2021-07-22 12:01:49 +0000

    chore(release): 1.26.1
    ## [1.26.1](https://github.com/RedHatInsights/patchman-ui/compare/v1.26.0...v1.26.1) (2021-07-22)
    
    ### Bug Fixes
    
    * **PackageSystems:** updatable key is added into reducer ([56987ad](https://github.com/RedHatInsights/patchman-ui/commit/56987ad5cdd90bd5cdca15f9710bfb954c81dfa9))

--- js/Register.ba4d23a8ad15662a0e9d.js
+++ js/Register.ba4d23a8ad15662a0e9d.js
@@ -1,2 +1,2 @@
 (self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman=self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman||[]).push([[494],{87810:(e,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>u});var r=t(86350),a=t(77865),l=t.n(a),c=t(49642),s=t(31507),_=t(32132),o=t(30893);const u=function(){return l().createElement(l().Fragment,null,l().createElement(c.Z,{title:_.N.formatMessage(o.Z.generalAppName)}),l().createElement(r.Z,null,l().createElement("div",null,l().createElement(s.Q,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/Register.f9f9f73e7c8eeeae69ab.js.map
+//# sourceMappingURL=../sourcemaps/Register.ac3a86a74ad4ff9e9592.js.map
