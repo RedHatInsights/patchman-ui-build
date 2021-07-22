777595d819aa36bd0f151afa9457d6e93fd5b4f2
Travis CI User <travis@example.org>
2021-07-22 10:37:40 +0000

    chore(release): 1.26.0
    # [1.26.0](https://github.com/RedHatInsights/patchman-ui/compare/v1.25.0...v1.26.0) (2021-07-22)
    
    ### Bug Fixes
    
    * **select:** Disable checkbox on up-to-date systems ([1325ec7](https://github.com/RedHatInsights/patchman-ui/commit/1325ec76ba43a5f5c06c037a4c71113887d22ada))
    * **select:** fix unselection of items on package-systems ([fafcbc4](https://github.com/RedHatInsights/patchman-ui/commit/fafcbc4c3f8d810da2383bf015472f8b67fe4dbf))
    
    ### Features
    
    * **PackageDetail:** SPM-961 enable remediation ([e8cd664](https://github.com/RedHatInsights/patchman-ui/commit/e8cd664ab3c2c957100f35c4bc7f848f16291b94))
    * **remediation:** Add provider for system-package remediation ([2e52da1](https://github.com/RedHatInsights/patchman-ui/commit/2e52da10b795dfc60479f553b47e6511292b0614))

--- js/Register.ba4d23a8ad15662a0e9d.js
+++ js/Register.ba4d23a8ad15662a0e9d.js
@@ -1,2 +1,2 @@
 (self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman=self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman||[]).push([[494],{87810:(e,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>u});var r=t(86350),a=t(77865),l=t.n(a),c=t(49642),s=t(31507),_=t(32132),o=t(30893);const u=function(){return l().createElement(l().Fragment,null,l().createElement(c.Z,{title:_.N.formatMessage(o.Z.generalAppName)}),l().createElement(r.Z,null,l().createElement("div",null,l().createElement(s.Q,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/Register.c3ec93cc4c198bc34cb1.js.map
+//# sourceMappingURL=../sourcemaps/Register.684e1c59d19abcdd994e.js.map
