8c7bccf3322c89ad8d58ae87a6b19f6e1ca2b850
Travis CI User <travis@example.org>
2021-08-19 11:47:43 +0000

    fix(Filter Chips): SPM-1095 make search filter chip text consistent with search filter title

--- js/NoAccess.f038cc4108ce8bd45734.js
+++ js/NoAccess.f038cc4108ce8bd45734.js
@@ -1,2 +1,2 @@
 (self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman=self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman||[]).push([[362],{18008:(e,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>u});var r=t(86350),a=t(77865),c=t.n(a),o=t(49642),l=t(32132),s=t(30893),_=t(73888),m=t(45697),p=function(e){var n=e.code;return c().createElement(c().Fragment,null,c().createElement(o.Z,{title:l.N.formatMessage(s.Z.generalAppName)}),c().createElement(r.Z,null,c().createElement(_.Z,{code:n})))};p.propTypes={code:t.n(m)().number};const u=p}}]);
-//# sourceMappingURL=../sourcemaps/NoAccess.209ea2c7386da2b09b32.js.map
+//# sourceMappingURL=../sourcemaps/NoAccess.e3da06d64705abf026d6.js.map
