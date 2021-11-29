57faa0a53c318463bcc0774d642e51573ec90e32
Travis CI User <travis@example.org>
2021-11-29 13:26:29 +0000

    chore(release): 1.38.0
    # [1.38.0](https://github.com/RedHatInsights/patchman-ui/compare/v1.37.1...v1.38.0) (2021-11-29)
    
    ### Features
    
    * **PackageSystems:** SPM-1178 add tag filter and column ([aa0c1a1](https://github.com/RedHatInsights/patchman-ui/commit/aa0c1a126539dece1bf72d2589ad0011da5f6e20))
    * **Tags:** SPM-1178 add tags column, tag filter ([3653e2b](https://github.com/RedHatInsights/patchman-ui/commit/3653e2bcfec6a8b10766053b1964eb92ab950fbf))

---
+++ js/833.6f7bc30fcdd94ee7cc50.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman=self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman||[]).push([[833],{45467:()=>{},34946:()=>{},32857:()=>{},28992:()=>{},30187:()=>{},90479:()=>{},78752:()=>{},21626:()=>{},66822:()=>{},49854:()=>{}}]);
