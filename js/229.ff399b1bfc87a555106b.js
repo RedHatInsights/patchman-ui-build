b30b5a6231fa35ac0d5e7927aa9aec49fa7bd653
Travis CI User <travis@example.org>
2021-12-08 12:50:18 +0000

    fix(modules): unique module names introduced, should fix caching issues

--- js/229.ff399b1bfc87a555106b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman=self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman||[]).push([[229],{54994:()=>{},37494:()=>{},67761:()=>{}}]);
--- js/392.f58bd8e04704ed669d9e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman=self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman||[]).push([[392],{81754:()=>{},80598:()=>{},74181:()=>{},11177:()=>{}}]);
