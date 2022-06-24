c48d20a3800259f7606da2c0c7975ee5d16c8f84
Travis CI User <travis@example.org>
2022-06-24 08:43:31 +0000

    chore(tests): update snapshots

---
+++ js/3398.1656060023903.405f6168e57b6cf8b100.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman=self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman||[]).push([[3398],{93398:()=>{}}]);
