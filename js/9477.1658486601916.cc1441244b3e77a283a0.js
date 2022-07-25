4fd9394b0c26d14c2710c351a37aca39fc78370a
Travis CI User <travis@example.org>
2022-07-25 10:48:21 +0000

    chore(deps): use explicit version of patternfly-react-core

--- js/9477.1658486601916.cc1441244b3e77a283a0.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman=self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman||[]).push([[9477,5467,3929,9682],{45467:()=>{}}]);
---
+++ js/9630.1658745898307.b84833177b2c8addc218.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman=self.webpackChunk_redhat_cloud_services_frontend_components_inventory_patchman||[]).push([[9630,4543,7537,6363],{74543:()=>{}}]);
