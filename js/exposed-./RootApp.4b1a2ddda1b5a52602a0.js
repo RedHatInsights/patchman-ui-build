d510a28736bfba8d301f5b1660cc2175262aed83
Travis CI User <travis@example.org>
2023-07-03 08:13:14 +0000

    Update frontend-components-config to fix crash on all pages

---
+++ js/exposed-./RootApp.4b1a2ddda1b5a52602a0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpatch=self.webpackChunkpatch||[]).push([[8178],{28412:(e,t,a)=>{a.r(t),a.d(t,{default:()=>o});var c=a(43297),l=a.n(c),n=a(28216),s=a(56909),r=a(7322),u=a(55323),h=a(59748);const o=function(){return l().createElement("div",{className:"patch-root"},l().createElement(u.Pj,{locale:navigator.language.slice(0,2),messages:h},l().createElement(n.zt,{store:r.h},l().createElement(s.Z,null))))}}}]);
+//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.d0d29a096efcf8181d4d0b1e6d496540.js.map
